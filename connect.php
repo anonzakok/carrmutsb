b98a5f443d2dd65f6b48bf99004530e5e2443758
anonzakok <55388823+anonzakok@users.noreply.github.com>
2020-11-10 18:27:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = new mysqli('localhost','root','','car_rmutsb2');
+    $conn = new mysqli('localhost','root','password','car_rmutsb');
 
       if( $conn->connect_errno){
              die("Connection failed" . $conn->connect_error); 
